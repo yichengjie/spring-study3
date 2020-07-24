d7ab7ac20ecc5ce203653f8b8b51aca7672f0641
易成杰 <626659321@qq.com>
2020-07-24 21:58:51 +0800

    自定义PropertyEditor

---
+++ spring-ioc-study/src/main/java/com/yicj/study/ioc/propertyeditor/DateFoo.java
@@ -0,0 +1,13 @@
+package com.yicj.study.ioc.propertyeditor;
+
+/**
+ * ClassName: DateFoo
+ * Description: TODO(描述)
+ * Date: 2020/7/24 21:44
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class DateFoo {
+}
