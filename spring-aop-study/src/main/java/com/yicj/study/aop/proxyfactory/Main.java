c29e7152ae4a0f67d159cd229c543c90624c0306
易成杰 <626659321@qq.com>
2020-07-31 20:28:24 +0800

    补充测试

--- spring-aop-study/src/main/java/com/yicj/study/aop/proxyfactory/Main.java
+++ spring-aop-study/src/main/java/com/yicj/study/aop/proxyfactory/Main.java
@@ -1,9 +1,12 @@
 package com.yicj.study.aop.proxyfactory;
 
+import com.yicj.study.aop.advice.CountingAfterAdvice;
 import com.yicj.study.aop.advice.CountingBeforeAdvice;
+import com.yicj.study.aop.advice.CountingThrowsAdvice;
 import com.yicj.study.aop.foo.IHello;
 import com.yicj.study.aop.foo.impl.HelloImpl;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 
 /**
  * ClassName: Main
@@ -18,21 +21,27 @@ public class Main {
 
     public static void main(String[] args) {
         IHello target = new HelloImpl() ;
-        CountingBeforeAdvice advice  = new CountingBeforeAdvice();
+        CountingBeforeAdvice beforeAdvice  = new CountingBeforeAdvice();
+        CountingAfterAdvice afterAdvice  = new CountingAfterAdvice();
+        CountingThrowsAdvice throwsAdvice  = new CountingThrowsAdvice();
+
+        DefaultPointcutAdvisor beforeAdvisor = new DefaultPointcutAdvisor(beforeAdvice) ;
         ProxyFactory pf = new ProxyFactory() ;
         // 指定针对接口代理
         pf.setInterfaces(target.getClass().getInterfaces());
         pf.setTarget(target);
-        pf.addAdvice(advice);
+        pf.addAdvisor(beforeAdvisor);
+        pf.addAdvice(afterAdvice);
+        pf.addAdvice(throwsAdvice);
 
         IHello proxy = (IHello) pf.getProxy();
-//        proxy.hello("张三") ;
+        proxy.hello("张三") ;
 //        proxy.hello("李四") ;
 //        proxy.hello("王五") ;
 //        proxy.hello("赵六") ;
 
 
-        int calls = advice.getCalls();
+        int calls = beforeAdvice.getCalls();
         System.out.println("所有方法调用总和：" + calls);
     }
 }
