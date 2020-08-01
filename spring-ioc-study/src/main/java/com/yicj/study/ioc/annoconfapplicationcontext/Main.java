15b3d6341536b5fef0b1db89e9a59883625551ee
易成杰 <626659321@qq.com>
2020-08-01 21:51:05 +0800

    保存

---
+++ spring-ioc-study/src/main/java/com/yicj/study/ioc/annoconfapplicationcontext/Main.java
@@ -0,0 +1,43 @@
+package com.yicj.study.ioc.annoconfapplicationcontext;
+
+import com.yicj.study.ioc.foo.config.AppBeanConfiguration;
+import com.yicj.study.ioc.foo.service.FXNewsProvider;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * ClassName: Main
+ * Description: TODO(描述)
+ * Date: 2020/8/1 21:33
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        testAnno() ;
+    }
+
+
+    public static void testAnno(){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppBeanConfiguration.class);
+        FXNewsProvider newsProvider = (FXNewsProvider)context.getBean("newsProvider");
+        System.out.println(newsProvider);
+        System.out.println(newsProvider.getNewsListener());
+        System.out.println(newsProvider.getNewsPersistent());
+    }
+
+    public static void testRegister(){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppBeanConfiguration.class);
+        // application.scan("com.yicj.study") ;
+        // must use refresh() method
+        context.refresh();
+        FXNewsProvider newsProvider = (FXNewsProvider)context.getBean("newsProvider");
+
+        System.out.println(newsProvider);
+        System.out.println(newsProvider.getNewsListener());
+        System.out.println(newsProvider.getNewsPersistent());
+    }
+}
---
+++ spring-ioc-study/src/main/java/com/yicj/study/ioc/foo/config/AppBeanConfiguration.java
@@ -0,0 +1,51 @@
+package com.yicj.study.ioc.foo.config;
+
+import com.yicj.study.ioc.foo.service.FXNewsProvider;
+import com.yicj.study.ioc.foo.service.IFXNewsListener;
+import com.yicj.study.ioc.foo.service.IFXNewsPersistent;
+import com.yicj.study.ioc.foo.service.impl.DowJonesNewListener;
+import com.yicj.study.ioc.foo.service.impl.DowJonesNewsPersistent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName: AppBeanConfiguration
+ * Description: TODO(描述)
+ * Date: 2020/8/1 21:39
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+@Configuration
+public class AppBeanConfiguration {
+
+    @Bean
+    public IFXNewsListener newsListener(){
+        System.out.println("-------newsListener()---------");
+        return new DowJonesNewListener() ;
+    }
+
+    @Bean
+    public IFXNewsPersistent newsPersistent(){
+        System.out.println("-------newsPersistent()---------");
+        return new DowJonesNewsPersistent() ;
+    }
+
+    @Bean
+    public FXNewsProvider newsProvider(IFXNewsListener newsListener, IFXNewsPersistent newsPersistent){
+        FXNewsProvider newsProvider = new FXNewsProvider();
+        newsProvider.setNewsListener(newsListener);
+        newsProvider.setNewsPersistent(newsPersistent);
+        return newsProvider ;
+    }
+
+    @Bean
+    public FXNewsProvider newsProvider2(){
+        FXNewsProvider newsProvider = new FXNewsProvider();
+        newsProvider.setNewsListener(newsListener());
+        newsProvider.setNewsPersistent(newsPersistent());
+        return newsProvider ;
+    }
+
+}
