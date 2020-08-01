7c7bbda1602e83293180a5e4db94c8ea4e88650c
易成杰 <626659321@qq.com>
2020-08-01 18:28:30 +0800

    保存

---
+++ spring-ioc-study/src/main/java/com/yicj/study/ioc/createbeanviacode/Main.java
@@ -0,0 +1,32 @@
+package com.yicj.study.ioc.createbeanviacode;
+
+import com.yicj.study.ioc.foo.service.FXNewsProvider;
+import com.yicj.study.ioc.foo.service.IFXNewsListener;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * ClassName: Main
+ * Description: TODO(描述)
+ * Date: 2020/8/1 17:14
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //AutowireCapableBeanFactory
+        String configLocation = "beans.xml" ;
+        ApplicationContext context = new ClassPathXmlApplicationContext(configLocation, Main.class) ;
+        AutowireCapableBeanFactory beanFactory = context.getAutowireCapableBeanFactory();
+        FXNewsProvider provider  = beanFactory.createBean(FXNewsProvider.class);
+        System.out.println("====> " + provider);
+        IFXNewsListener newsListener = provider.getNewsListener();
+        System.out.println("====> " + newsListener);
+        //Object fXNewsProvider = context.getBean("fXNewsProvider");
+        //System.out.println(fXNewsProvider);
+    }
+}
