0b608dd550d69d7152c31d67696f738de382a7ea
易成杰 <626659321@qq.com>
2020-08-02 19:24:32 +0800

    保存

--- spring-mvc-study/src/main/java/com/yicj/study/mvc/foo/hello/config/WebConfig.java
+++ spring-mvc-study/src/main/java/com/yicj/study/mvc/foo/hello/config/WebConfig.java
@@ -3,10 +3,12 @@ package com.yicj.study.mvc.foo.hello.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -22,6 +24,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         resolver.setExposeContextBeansAsAttributes(true);
         return resolver;
     }
+
+//    @Bean
+//    public HandlerMapping handlerMapping(){
+//        SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping() ;
+//
+//        return handlerMapping ;
+//    }
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
