8f7f8cab9e13228b595b2d2c660c3744ffdba39e
易成杰 <626659321@qq.com>
2020-08-08 11:14:53 +0800

    保存

--- spring-mvc-study/src/main/java/com/yicj/study/mvc/foo/hello/controller/HomeController.java
+++ spring-mvc-study/src/main/java/com/yicj/study/mvc/foo/hello/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.yicj.study.mvc.foo.hello.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
+@Slf4j
 public class HomeController {
- 
+
+    @Autowired
+    private ApplicationContext applicationContext ;
+
     @RequestMapping(value="/hello", method=GET)
     public String home() {
+        log.info("======> applicationContext :{}" ,applicationContext);
         return "home";
     }
  
